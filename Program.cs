48ebd6e85f8f43912e4f477580653100556b1b7d
mbrajk <mbrajk@users.noreply.github.com>
2024-03-28 00:14:20 -0500

    use spectre cli to allow for filtering on budget and category via args

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.ComponentModel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Refit;
 using Spectre.Console;
@@ -6,52 +7,75 @@ using BudgetSync.YnabApi;
 using BudgetSync.YnabApi.Budget;
 using Polly;
 using Polly.Extensions.Http;
+using Spectre.Console.Cli;
 using ynac;
 
-var config =  new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-var services = BuildServiceProvider(config);
+var app = new CommandApp<BudgetCommand>();
+await app.RunAsync(args);
 
-ServiceProvider BuildServiceProvider(IConfigurationRoot configurationRoot)
+public static class Setup
 {
-	var services = new ServiceCollection();
+	public static ServiceProvider BuildServiceProvider(IConfigurationRoot configurationRoot)
+	{
+		var services = new ServiceCollection();
 
-	// refit http client
-	services
-		.AddRefitClient<IYnabApi>()
-		.ConfigureHttpClient(
-			httpClient =>
-			{
-				var endpoint = config["YnabApi:Endpoint"];
-				var version = config["YnabApi:Version"];
-				var token = config["YnabApi:Token"];
+		// refit http client
+		services
+			.AddRefitClient<IYnabApi>()
+			.ConfigureHttpClient(
+				httpClient =>
+				{
+					var endpoint = configurationRoot["YnabApi:Endpoint"];
+					var version = configurationRoot["YnabApi:Version"];
+					var token = configurationRoot["YnabApi:Token"];
 
-				httpClient.BaseAddress = new Uri($"{endpoint}/{version}");
-				httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-			}
-		)
-		.AddPolicyHandler(
-			HttpPolicyExtensions
-				.HandleTransientHttpError()
-				.OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
-				.WaitAndRetryAsync(4, retry => TimeSpan.FromSeconds(Math.Pow(2, retry)))
-		);
+					httpClient.BaseAddress = new Uri($"{endpoint}/{version}");
+					httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+				}
+			)
+			.AddPolicyHandler(
+				HttpPolicyExtensions
+					.HandleTransientHttpError()
+					.OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+					.WaitAndRetryAsync(4, retry => TimeSpan.FromSeconds(Math.Pow(2, retry)))
+			);
 
-	// other required dependencies
-	services.AddSingleton<IYnabConsole, Ynac>();
-	services.AddSingleton<IBudgetQueryService, BudgetQueryService>();
+		// other required dependencies
+		services.AddSingleton<IYnabConsole, Ynac>();
+		services.AddSingleton<IBudgetQueryService, BudgetQueryService>();
 
-	return services.BuildServiceProvider();
+		return services.BuildServiceProvider();
+	}
 }
 
-var budgetApplication = services.GetRequiredService<IYnabConsole>();
+public sealed class BudgetCommand : AsyncCommand<BudgetCommand.Settings>
+{
+	public sealed class Settings : CommandSettings
+	{
+		[Description("The budget name to filter to. Will find the first budget that contains this string")]
+		[CommandArgument(0, "[budgetFilter]")]
+		public string? BudgetFilter { get; init; }
 
-await budgetApplication.RunAsync();
+		[Description("The category name to filter to. Will find the first category containing this string")]
+		[CommandArgument(0, "[budgetFilter]")]
+		public string? CategoryFilter { get; init; }
+	}
+
+	public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+	{
+		var config =  new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+		var services = Setup.BuildServiceProvider(config);
+		var ynabConsole = services.GetRequiredService<IYnabConsole>();
+		await ynabConsole.RunAsync(settings.BudgetFilter, settings.CategoryFilter);
+		return 0;
+	}
+}
 
 namespace ynac
 {
 	public interface IYnabConsole
 	{
-		public Task RunAsync();
+		public Task RunAsync(string? budgetFilter, string? categoryFilter);
 	}
 
 	class Ynac : IYnabConsole
@@ -62,18 +86,35 @@ namespace ynac
 		{
 			_budgetQueryService = budgetQueryService;
 		}
-		public async Task RunAsync()
+		public async Task RunAsync(string? budgetFilter, string? categoryFilter)
 		{
-			var budgets = _budgetQueryService.GetBudgets();
-			var index = 0;
-			AnsiConsole.Markup("[bold aqua]            You Need A Console[/]\n");
-			foreach (var budget in budgets.Result)	
+			var budgets = await _budgetQueryService.GetBudgets();
+			Budget selectedBudget = null;
+			
+			if (string.IsNullOrWhiteSpace(budgetFilter))
 			{
-				AnsiConsole.Markup($"[underline yellow]{index++}[/] [bold white]{budget.Name}[/] [italic grey]{budget.Id}[/]\n");
+				var index = 0;
+				AnsiConsole.Markup("[bold aqua]            You Need A Console[/]\n");
+				foreach (var budget in budgets)
+				{
+					AnsiConsole.Markup(
+						$"[underline yellow]{index++}[/] [bold white]{budget.Name}[/] [italic grey]{budget.Id}[/]\n");
+				}
+
+				var selection = AnsiConsole.Ask<string>("[italic grey]   Select a budget:[/] ");
+				int.TryParse(selection, out var selectionIndex);
+				selectedBudget = budgets.ElementAt(selectionIndex);
 			}
-			var selection = AnsiConsole.Ask<string>("[italic grey]   Select a budget:[/] ");
-			int.TryParse(selection, out var selectionIndex);
-			var selectedBudget = budgets.Result.ElementAt(selectionIndex);
+			else
+			{
+				selectedBudget = budgets.FirstOrDefault(b => b.Name.Contains(budgetFilter, StringComparison.OrdinalIgnoreCase));
+			}
+
+			if (selectedBudget == null)
+			{
+				AnsiConsole.Markup("[red]Budget(s) not found[/]");
+				return;
+			}
 			
 			var budgetCategoryGroups = await _budgetQueryService.GetBudgetCategories(selectedBudget);
 			var table = new Table()
@@ -81,8 +122,13 @@ namespace ynac
 				.Border(TableBorder.Rounded)
 				.BorderColor(Color.Yellow);	
 			table.AddColumn($"[bold white][[[/] [yellow]{selectedBudget.Name}[/] [bold white]]][/]");
-			
-			foreach (var categoryGroup in budgetCategoryGroups.Where(c => !c.Hidden).Skip(1).SkipLast(1))
+
+			var categoryGroups = budgetCategoryGroups.Where(c => !c.Hidden).Skip(1).SkipLast(1);
+			if (!string.IsNullOrWhiteSpace(categoryFilter))
+			{
+				categoryGroups = categoryGroups.Where(c => c.Name.Contains(categoryFilter, StringComparison.OrdinalIgnoreCase));
+			}
+			foreach (var categoryGroup in categoryGroups)
 			{
 				var subTable = new Table()
 					.Title(categoryGroup.Name, Style.Parse("italic blue"))
@@ -99,7 +145,7 @@ namespace ynac
 				foreach (var category in categoryGroup.Categories.Where(c => !c.Hidden))
 				{
 					var activityDollars = category.Activity / 1000;
-					var budgetedDollars = category.Balance / 1000;
+					var budgetedDollars = category.Budgeted / 1000;
 					var availableDollars = category.Balance / 1000;
 					
 					totalBudgeted += budgetedDollars;
@@ -113,13 +159,12 @@ namespace ynac
 						$"[green]{availableDollars.ToString("C")}[/]"
 					);
 				}
-					
-				subTable.AddRow(
+				subTable.ShowFooters().AddRow(
 					"", 
 					$"[green]{totalBudgeted.ToString("C")}[/]", 
 					$"[red]{totalActivity.ToString("C")}[/]", 
 					$"[green]{totalAvailable.ToString("C")}[/]"
-				);
+				).ShowRowSeparators().MinimalBorder();
 
 				table.AddRow(subTable);
 			}
