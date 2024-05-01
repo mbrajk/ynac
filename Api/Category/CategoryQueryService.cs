28f8a889bfa4e37f5be91d77c8b50187e725fd32
Mike Brajkovich <mbrajk@users.noreply.github.com>
2024-05-01 16:39:12 -0500

    add "last-used" as a budget choice (#25)

--- Api/Category/CategoryQueryService.cs
+++ Api/Category/CategoryQueryService.cs
@@ -4,7 +4,7 @@
     {
         public async Task<IReadOnlyCollection<CategoryGroup>> GetBudgetCategoriesAsync(Budget.Budget budget)
         {
-            var response = await ynabApi.GetBudgetCategoriesAsync(budget.Id);
+            var response = await ynabApi.GetBudgetCategoriesAsync(budget.BudgetId);
             
             return response.Data?.Groups ?? new []{ new CategoryGroup() };
         }
