70e2f7b9dfa36c0fd61c58e556e0ee904c1a42d8
mbrajk <mbrajk@users.noreply.github.com>
2024-05-06 22:23:13 -0500

    exclude last used budget from filterered list or when --open is passed as an argument

--- Api/Budget/IBudgetQueryService.cs
+++ Api/Budget/IBudgetQueryService.cs
@@ -4,7 +4,7 @@ namespace YnabApi.Budget
 {
     public interface IBudgetQueryService
     {
-        Task<IReadOnlyCollection<Budget>> GetBudgets(string? filter);
+        Task<IReadOnlyCollection<Budget>> GetBudgets(string? bugdetFilter, bool hideLastUsedBudget = false);
         Task<BudgetMonth> GetBudgetMonth(Budget budget, DateOnly date);
         Task<BudgetMonth> GetCurrentMonthBudget(Budget selectedBudget);
         Task<IReadOnlyCollection<CategoryGroup>> GetBudgetCategories(Action<BudgetCategorySearchOptions> configureOptions);
