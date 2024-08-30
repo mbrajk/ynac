e2e9d8c66a0890fa4ec8ffdaea3d39acba345557
mbrajk <mbrajk@users.noreply.github.com>
2024-08-29 23:04:08 -0500

    creating a wrapper for static prompt for testing

--- ynac.cli/BudgetSelection/BudgetPrompter.cs
+++ ynac.cli/BudgetSelection/BudgetPrompter.cs
@@ -3,8 +3,33 @@ using YnabApi.Budget;
 
 namespace ynac.BudgetSelection;
 
+public interface IConsolePrompt
+{
+    T Prompt<T>(SelectionPrompt<T> prompt) where T : notnull;
+}
+
+public class ConsolePrompt : IConsolePrompt
+{
+    private readonly IAnsiConsole _console;
+
+    public ConsolePrompt(IAnsiConsole console)
+    {
+        _console = console;
+    }
+
+    public T Prompt<T>(SelectionPrompt<T> prompt) where T : notnull
+    {
+        return _console.Prompt(prompt);
+    }
+}
+
 public class BudgetPrompter : IBudgetPrompter
 {
+    private readonly IConsolePrompt _console;
+    public BudgetPrompter(IConsolePrompt console)
+    {
+        _console = console;
+    }
     public Budget PromptBudgetSelection(IReadOnlyCollection<Budget> budgets)
     {
         if (budgets.Count == 0)
@@ -18,7 +43,7 @@ public class BudgetPrompter : IBudgetPrompter
             return budgets.First();
         }
             		
-        var selectedBudget = AnsiConsole.Prompt(
+        var selectedBudget = _console.Prompt(
             new SelectionPrompt<Budget>()
                 .Title("[italic grey]Select a[/] [italic aqua]budget:[/]")
                 .PageSize(10)
