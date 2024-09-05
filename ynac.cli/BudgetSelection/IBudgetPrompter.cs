a0fa4ad6fded36e41743889cf7249e8e8cd4969e
mbrajk <mbrajk@users.noreply.github.com>
2024-09-05 10:00:32 -0500

    Wrapper for testing

--- ynac.cli/BudgetSelection/IBudgetPrompter.cs
+++ ynac.cli/BudgetSelection/IBudgetPrompter.cs
@@ -2,7 +2,7 @@ using YnabApi.Budget;
 
 namespace ynac.BudgetSelection;
 
-public interface IBudgetPrompter
+public interface IBudgetPrompter : IPrompter
 {
     public Budget PromptBudgetSelection(IReadOnlyCollection<Budget> budgets);
 }
