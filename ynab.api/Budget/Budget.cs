96d3c7453cfeb65ed5e3aa3100a288236cef59ab
mbrajk <mbrajk@users.noreply.github.com>
2025-02-18 23:47:32 -0600

    add missing JsonProperyName attributes to api model properties

--- ynab.api/Budget/Budget.cs
+++ ynab.api/Budget/Budget.cs
@@ -32,6 +32,7 @@ namespace YnabApi.Budget
         [JsonPropertyName("name")]
         public string Name { get; init; } = string.Empty;
 
+        [JsonPropertyName("id")]
         public Guid Id { get; init; }
         
         public BudgetType Type { get; init; } = BudgetType.UserBudget;
