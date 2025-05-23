75f0d614ea8c0f3119c995b709d80fe19bc9c045
mbrajk <mbrajk@users.noreply.github.com>
2025-05-23 09:46:16 -0500

    adding parsing attributes

--- ynab/Transaction/Transaction.cs
+++ ynab/Transaction/Transaction.cs
@@ -1,54 +1,74 @@
+using System.Text.Json.Serialization;
+
 namespace ynab.Transaction;
-public class RootObject
+public class Transaction
 {
-    public string id { get; set; }
-    public string date { get; set; }
-    public int amount { get; set; }
-    public object memo { get; set; }
-    public string cleared { get; set; }
-    public bool approved { get; set; }
-    public object flag_color { get; set; }
-    public object flag_name { get; set; }
-    public string account_id { get; set; }
-    public string account_name { get; set; }
-    public string payee_id { get; set; }
-    public string payee_name { get; set; }
-    public string category_id { get; set; }
-    public string category_name { get; set; }
-    public object transfer_account_id { get; set; }
-    public object transfer_transaction_id { get; set; }
-    public object matched_transaction_id { get; set; }
-    public string import_id { get; set; }
-    public string import_payee_name { get; set; }
-    public string import_payee_name_original { get; set; }
-    public object debt_transaction_type { get; set; }
-    public bool deleted { get; set; }
-    public object[] subtransactions { get; set; }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("date")]
+    public string Date { get; set; }
+    
+    [JsonPropertyName("amount")]
+    public int Amount { get; set; }
+    
+    [JsonPropertyName("memo")]
+    public string Memo { get; set; }
+    
+    [JsonPropertyName("cleared")]
+    public string Cleared { get; set; }
+    
+    [JsonPropertyName("approved")]
+    public bool Approved { get; set; }
+    
+    [JsonPropertyName("flag_color")]
+    public string FlagColor { get; set; }
+    
+    [JsonPropertyName("flag_name")]
+    public string FlagName { get; set; }
+    
+    [JsonPropertyName("account_id")]
+    public int AccountId { get; set; }
+    
+    [JsonPropertyName("account_name")]
+    public string AccountName { get; set; }
+    
+    [JsonPropertyName("payee_id")]
+    public int PayeeId { get; set; }
+    
+    [JsonPropertyName("payee_name")]
+    public string PayeeName { get; set; }
+    
+    [JsonPropertyName("category_id")]
+    public int CategoryId { get; set; }
+    
+    [JsonPropertyName("category_name")]
+    public string CategoryName { get; set; }
+    
+    [JsonPropertyName("transfer_account_id")]
+    public int TransferAccountId { get; set; }
+    
+    [JsonPropertyName("transfer_transaction_id")]
+    public int TransferTransactionId { get; set; }
+    
+    [JsonPropertyName("matched_transaction_id")]
+    public int MatchedTransactionId { get; set; }
+    
+    [JsonPropertyName("import_id")]
+    public string ImportId { get; set; }
+    
+    [JsonPropertyName("import_payee_name")]
+    public string ImportPayeeName { get; set; }
+    
+    [JsonPropertyName("import_payee_name_original")]
+    public string ImportPayeeNameOriginal { get; set; }
+    
+    [JsonPropertyName("debt_transaction_type")]
+    public string DebtTransactionType { get; set; }
+    
+    [JsonPropertyName("deleted")]
+    public bool Deleted { get; set; }
+    
+    [JsonPropertyName("subtransactions")]
+    public object[] Subtransactions { get; set; }
 }
-
-
-public record Transaction(
-    string Id,
-    string Date,
-    int Amount,
-    object Memo,
-    string Cleared,
-    bool Approved,
-    object FlagColor,
-    object FlagName,
-    string AccountId,
-    string AccountName,
-    string PayeeId,
-    string PayeeName,
-    string CategoryId,
-    string CategoryName,
-    object TransferAccountId,
-    object TransferTransactionId,
-    object MatchedTransactionId,
-    string ImportId,
-    string ImportPayeeName,
-    string ImportPayeeNameOriginal,
-    object DebtTransactionType,
-    bool Deleted
-);
-
