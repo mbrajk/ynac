4e66af150fd81b31554a7daadfce104ec2262a1a
mbrajk <mbrajk@users.noreply.github.com>
2025-08-07 23:19:00 -0500

    fix: force en-US for now to fix tests

--- ynac.cli/CurrencyFormatting/DefaultCurrencyFormatter.cs
+++ ynac.cli/CurrencyFormatting/DefaultCurrencyFormatter.cs
@@ -1,9 +1,17 @@
+using System.Globalization;
+
 namespace ynac.CurrencyFormatting;
 
 public class DefaultCurrencyFormatter : ICurrencyFormatter
 {
+    public string Format(decimal amount, CultureInfo? culture)
+    {
+        culture ??= CultureInfo.CurrentCulture;
+        return amount.ToString("C", culture);
+    }
+
     public string Format(decimal amount)
     {
-        return amount.ToString("C");
+        return Format(amount, CultureInfo.CurrentCulture);
     }
 }
