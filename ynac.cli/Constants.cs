2e016ecf94e26362c34230c01c176b1278bd5d50
mbrajk <mbrajk@users.noreply.github.com>
2026-01-07 21:28:46 -0600

    make config strings readonly

--- ynac.cli/Constants.cs
+++ ynac.cli/Constants.cs
@@ -6,23 +6,23 @@ public static class Constants
 {
     private const string YnabApiString = "YnabApi";
     private const string YnacString = "Ynac";
-   
-    internal static string YnabApiSectionHeader => $"[{YnabApiString}]";
-    internal static string TokenString => "Token";
-    internal static string YnabApiTokenConfigPath => $"{YnabApiString}:{TokenString}";
-    
-    internal static string YnacSectionHeader => $"[{YnacString}]";
-    internal static string HideAmountsString => "HideAmounts";
-    internal static string YnacHideAmountsConfigPath => $"{YnacString}:{HideAmountsString}";
-    
-    internal static string YnabRootUrl => "https://app.ynab.com/";
-    internal static string BudgetRouteAffix => "/budget";
-    
+
+    internal static readonly string YnabApiSectionHeader = $"[{YnabApiString}]";
+    internal static readonly string TokenString = "Token";
+    internal static readonly string YnabApiTokenConfigPath = $"{YnabApiString}:{TokenString}";
+
+    internal static readonly string YnacSectionHeader = $"[{YnacString}]";
+    internal static readonly string HideAmountsString = "HideAmounts";
+    internal static readonly string YnacHideAmountsConfigPath = $"{YnacString}:{HideAmountsString}";
+
+    internal static readonly string YnabRootUrl = "https://app.ynab.com/";
+    internal static readonly string BudgetRouteAffix = "/budget";
+
     // config.ini constants
-    internal static string ConfigFileName => "config.ini";
-    internal static string ConfigFileTemplate => "ynac._res.config.template.ini";
-    internal static string ConfigFilePath => Path.Combine(AppContext.BaseDirectory, ConfigFileName);
+    internal static readonly string ConfigFileName = "config.ini";
+    internal static readonly string ConfigFileTemplate = "ynac._res.config.template.ini";
+    internal static readonly string ConfigFilePath = Path.Combine(AppContext.BaseDirectory, ConfigFileName);
 
     // The default string to check for in config.ini, do not edit here 
-    internal static string DefaultTokenString => "put-your-token-here";
+    internal static readonly string DefaultTokenString = "put-your-token-here";
 }
